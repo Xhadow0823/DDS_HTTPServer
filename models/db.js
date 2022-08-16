f549fc2b8f684f4b43107d005932e76b610a720a
Eric's PC <44898836+Xhadow0823@users.noreply.github.com>
2022-08-16 14:03:11 +0800

    feat: 連接實驗室伺服器資料庫

--- models/db.js
+++ models/db.js
@@ -7,11 +7,15 @@ const database = process.env.db_database || 'DDS';
 const user = process.env.db_user || 'root';
 const pw = process.env.db_pw || 'password';
 const host = process.env.db_host || 'localhost';
+const port = process.env.db_port || 3306;
 
 const db = new Sequelize(database, user, pw, {
   host: host,
   dialect: 'mariadb',
-  dialectOptions: { connectTimeout: 1000 } // mariadb connector option
+  dialectOptions: {  // mariadb connector option
+    connectTimeout: 1000,
+    port: port
+  }
 });
 
 module.exports = db;
