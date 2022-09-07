11934e8a5dee9f90060d98a3924181b28306b346
Eric's MB <eric17618@gmail.com>
2022-09-07 16:31:58 +0800

    feat: 新增 socket.io 函式庫

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ dotenv.config();
 const api = require('./routes');
 
 const app = express();
+const { createServer } = require('http');
+const httpServer = createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(httpServer);
+
 const port = process.env.PORT;
 
 app.use(express.json());
@@ -23,6 +28,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+// 當有新 Socket 連接上時，做...
+io.on('connection', (socket) => {
+  console.log('New socket connection!');
+});
+
+httpServer.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 }); 
